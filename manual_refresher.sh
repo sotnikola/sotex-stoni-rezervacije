c4dc1888eb1c85d83591ac6c09d2912283eed9cd
sotnikola <nikolamilosavljevic@sotexsolutions.com>
2023-10-17 07:21:18 +0200

    updating ping

--- manual_refresher.sh
+++ manual_refresher.sh
@@ -1,8 +1,10 @@
 while true; do
+    now=$(date +"%T")
+    echo "Refreshing at ...$now"
     curl -X POST -H "Authorization: token $MASTER_KEY" \
         -H "Accept: application/vnd.github.everest-preview+json" \
         -d '{"ref": "main"}' \
         https://api.github.com/repos/sotnikola/sotex-stoni-rezervacije/actions/workflows/refresh.yaml/dispatches
-
-    sleep 180
+    echo "Sleeping for 5 minutes..."
+    sleep 300
 done
