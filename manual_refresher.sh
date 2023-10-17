6cded3771085c6bc7771fde327c9899eb6ddac0b
sotnikola <nikolamilosavljevic@sotexsolutions.com>
2023-10-17 07:03:03 +0200

    setting correct empty script name

--- manual_refresher.sh
+++ manual_refresher.sh
@@ -2,7 +2,7 @@ while true; do
     curl -X POST -H "Authorization: token $MASTER_KEY" \
         -H "Accept: application/vnd.github.everest-preview+json" \
         -d '{"ref": "main"}' \
-        https://api.github.com/repos/sotnikola/sotex-stoni-rezervacije/actions/workflows/Refresh%20job/dispatches
+        https://api.github.com/repos/sotnikola/sotex-stoni-rezervacije/actions/workflows/refresh.yaml/dispatches
 
     sleep 180
 done
